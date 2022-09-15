c99b57f5a3965f5b9dfb602dee1ebf1c2f1d7748
chris7695 <cschwennig@gmx.de>
2022-09-15 23:25:56 +0200

    Versuch der Implementierung vom Sinus

--- func-gen.c
+++ func-gen.c
@@ -14,6 +14,15 @@ void binaryGen(FILE *fh, int count) {
     }
 }
 
+void sinusGen(FILE *fh, int count) {
+    int i;
+
+    for (i = 0; count > i ; i++)
+    {
+        //fprintf(fh, "%d,%f\n",i, sin(i));
+    }
+}
+
 int main(int argc, char *argv[]) {
     int i;
     if (argc != 3) {
@@ -26,6 +35,7 @@ int main(int argc, char *argv[]) {
     fp = fopen(argv[2], "w+");
 
     binaryGen(fp, atoi(argv[1]));
+    //sinusGen(fp, atoi(argv[1]));
 
     fclose(fp);
 }
